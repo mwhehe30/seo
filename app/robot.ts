03b2f33872badbca0cbe3e3a0e181da561ed4705
zaky <zakymubarok05022009@gmail.com>
2025-12-09 12:48:01 +0700

    aa

--- app/robot.ts
+++ app/robot.ts
@@ -2,10 +2,17 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+      {
+        userAgent: 'Googlebot',
+        allow: '/',
+      },
+    ],
     sitemap: 'https://seo-ten-sand.vercel.app/sitemap.xml',
+    host: 'https://seo-ten-sand.vercel.app',
   };
 }
